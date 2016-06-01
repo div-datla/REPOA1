31a2fe99d007fe442e4feecacb6543ea9c0fb3e9
Divya Datla <ndatla3@gatech.edu>
2016-06-01 17:35:38 -0400

    Committing all files

---
+++ Assignment3/src/edu/gatech/seclass/MyCustomString.java
@@ -0,0 +1,97 @@
+package edu.gatech.seclass;
+
+
+
+public class MyCustomString implements MyCustomStringInterface{
+	
+	private String inputString;
+
+	@Override
+	public void setString(String string) {
+		this.inputString = string;
+		
+	}
+
+	@Override
+	public String getString() {
+		
+		return this.inputString;
+	}
+
+	@Override
+	public String getEveryNthCharacter(int n) throws MyIndexOutOfBoundsException{
+		int i =1;
+		int index = i*n - 1;
+		String mystring = this.inputString;
+		String newstring = "";
+		char letter= '0';
+		
+		if (n<1){
+			throw new MyIndexOutOfBoundsException();
+			}
+	
+		while (index< mystring.length()) {
+			letter = mystring.charAt(index);
+			newstring = newstring + Character.toString(letter);
+			i++;
+			index = i*n - 1;
+			
+		}
+		
+		//else {return "n must be greater than 0";}
+			
+		return newstring;
+	}
+
+	@Override
+	public int countDigits() {
+		int countDigits=0;
+		char nthChar;
+		String inputString = this.inputString;
+		for (int i =0;i< inputString.length(); i++) {
+			
+			nthChar = inputString.charAt(i);
+			if (Character.isDigit(nthChar)){
+				countDigits++;
+			}
+			
+		}
+		return countDigits;
+	}
+
+	@Override
+	public void convertDigitsToNamesInSubstring(int startPosition, int endPosition)
+			throws MyIndexOutOfBoundsException, IllegalArgumentException {
+		// TODO Auto-generated method stub
+		 if (startPosition > endPosition){
+			throw new IllegalArgumentException();
+			}
+		 if (startPosition <1 || endPosition > this.inputString.length()) {
+			 throw new MyIndexOutOfBoundsException();
+		 }
+		 
+			
+			
+			
+			String myString = inputString.substring(startPosition-1, endPosition);
+			System.out.println("Before:" +  myString);
+			myString = myString.replaceAll("10", "Ten");
+			myString = myString.replaceAll("0", "Zero");
+			myString = myString.replaceAll("1", "One");
+			myString = myString.replaceAll("2", "Two");
+			myString = myString.replaceAll("3", "Three");
+			myString = myString.replaceAll("4", "Four");
+			myString = myString.replaceAll("5", "Five");
+			myString = myString.replaceAll("6", "Six");
+			myString = myString.replaceAll("7", "Seven");
+			myString = myString.replaceAll("8", "Eight");
+			myString = myString.replaceAll("9", "Nine");
+			System.out.println("After:" +  myString);
+			
+			this.inputString = inputString.substring(0, startPosition-1) + myString 
+					+ inputString.substring(endPosition, inputString.length());
+			
+		
+	}
+
+}
---
+++ Assignment3/test/edu/gatech/seclass/MyCustomStringTest.java
@@ -0,0 +1,121 @@
+package edu.gatech.seclass;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyCustomStringTest {
+
+	private MyCustomStringInterface mycustomstring;
+
+	@Before
+	public void setUp() throws Exception {
+		mycustomstring = new MyCustomString();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		mycustomstring = null;
+	}
+
+    @Test
+    public void testCountDigits1() {
+        mycustomstring.setString("I'd better put s10me d1gits in this 5tr1n9, right?");
+        assertEquals(6, mycustomstring.countDigits());
+    }
+
+	@Test
+	public void testCountDigits2() {
+		mycustomstring.setString("1234567890");
+        assertEquals(10, mycustomstring.countDigits());
+	}
+
+	@Test
+	public void testCountDigits3() {
+		mycustomstring.setString("There are no numbers in this string");
+        assertEquals(0, mycustomstring.countDigits());
+	}
+
+	@Test
+	public void testCountDigits4() {
+		mycustomstring.setString("");
+        assertEquals(0, mycustomstring.countDigits());
+	}
+
+    @Test
+    public void testGetEveryNthCharacter1() {
+        mycustomstring.setString("I'd better put s10me d1gits in this 5tr1n9, right?");
+        assertEquals("deep 0 gsnh r9rh", mycustomstring.getEveryNthCharacter(3));
+    }
+
+	@Test (expected = MyIndexOutOfBoundsException.class)
+	public void testGetEveryNthCharacter2() { 
+		 mycustomstring.setString("I'd better put s10me d1gits in this 5tr1n9, right?");
+	        assertEquals("Throw Exception", mycustomstring.getEveryNthCharacter(0));
+	}
+
+	@Test
+	public void testGetEveryNthCharacter3() { fail("Not yet implemented"); }
+
+	@Test
+	public void testGetEveryNthCharacter4() {
+		fail("Not yet implemented");
+	}
+
+    @Test
+    public void testGetEveryNthCharacter5() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testConvertDigitsToNamesInSubstring1() {
+        mycustomstring.setString("I'd better put s10me d1gits in this 5tr1n9, right?");
+        mycustomstring.convertDigitsToNamesInSubstring(17, 23);
+        assertEquals("I'd better put sTenme dOnegits in this 5tr1n9, right?", mycustomstring.getString());
+    }
+
+    @Test
+    public void testConvertDigitsToNamesInSubstring2() { 
+    	mycustomstring.setString("2his string has no digits");
+        mycustomstring.convertDigitsToNamesInSubstring(1, 1);
+        assertEquals("Twohis string has no digits", mycustomstring.getString());
+        }
+
+    @Test
+    public void testConvertDigitsToNamesInSubstring3() { 
+    	mycustomstring.setString("011001");
+        mycustomstring.convertDigitsToNamesInSubstring(1,6);
+        assertEquals("ZeroOneTenZeroOne", mycustomstring.getString());
+        }
+
+    @Test
+    public void testConvertDigitsToNamesInSubstring4() { 
+    	mycustomstring.setString("011001");
+        mycustomstring.convertDigitsToNamesInSubstring(6,6);
+        assertEquals("01100One", mycustomstring.getString());
+        }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertDigitsToNamesInSubstring5() { 
+    	mycustomstring.setString("I'd better put s10me d1gits in this 5tr1n9, right?");
+    	mycustomstring.convertDigitsToNamesInSubstring(23, 17);
+    	assertEquals("I'd better put sTenme dOnegits in this 5tr1n9, right?", mycustomstring.getString());
+    }
+    
+    @Test(expected = MyIndexOutOfBoundsException.class)
+    public void testConvertDigitsToNamesInSubstring7() { 
+    	mycustomstring.setString("I'd better put s10me d1gits in this 5tr1n9, right?");
+    	mycustomstring.convertDigitsToNamesInSubstring(0, 0);
+    	assertEquals("I'd better put sTenme dOnegits in this 5tr1n9, right?", mycustomstring.getString());
+    }
+
+    @Test(expected = MyIndexOutOfBoundsException.class)
+    public void testConvertDigitsToNamesInSubstring6() { 
+    	mycustomstring.setString("SmallString");
+    	mycustomstring.convertDigitsToNamesInSubstring(17,23);
+    	assertEquals("I'd better put sTenme dOnegits in this 5tr1n9, right?", mycustomstring.getString());
+    	}
+
+}
