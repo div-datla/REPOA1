d857377a434a229bd05674f77249bb689083a989
Divya Datla <ndatla3@gatech.edu>
2016-06-07 14:14:22 -0400

    Screen Added

--- Assignment4/Converter/app/src/main/java/seclass/gatech/edu/converter/MainActivity.java
+++ Assignment4/Converter/app/src/main/java/seclass/gatech/edu/converter/MainActivity.java
@@ -10,4 +10,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String FromStones(String inputWeight){
+        return null;
+    }
+
+    public String FromPounds(String inputWeight){
+        return null;
+    }
+
+    public String FromOunces(String inputWeight){
+        return null;
+    }
+
+    public String FromGrains(String inputWeight){
+        return null;
+    }
+
+    public String FromMerticTons(String inputWeight){
+        return null;
+    }
+
+    public String FromKilograms(String inputWeight){
+        return null;
+    }
+
+    public String FromGrams(String inputWeight){
+        return null;
+    }
+
+    public String FromMilligrams(String inputWeight){
+        return null;
+    }
 }
